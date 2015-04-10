3efbe19d1ea520ec1c80616633d8c7bf1e14f3fd
unknown <filinale@KIEDWM417488.INTRANET.BARCAPINT.com>
2015-04-10 16:58:20 +0300

    Created data model

---
+++ SDesk/Epam.Sdesk.DBModel/Incident.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Sdesk.DBModel
+{
+    public class Incident
+    {
+        public Guid Id { get; set; }
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public string Priority { get; set; }
+        public string Impact { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public string Status { get; set; }
+        public string Assignee { get; set; }
+    }
+}
