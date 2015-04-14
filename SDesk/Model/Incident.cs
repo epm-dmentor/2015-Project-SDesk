e8f5605b7bc287d586ef121e7c17a04431359a05
Igor Tkachenko <legart.ua@gmail.com>
2015-04-14 21:00:44 +0300

    Reviewed

--- SDesk/Model/Incident.cs
+++ SDesk/Model/Incident.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Epam.Sdesk.DBModel
+namespace Epam.Sdesk.Model
 {
     public class Incident
     {
