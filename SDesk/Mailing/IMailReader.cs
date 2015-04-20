78bb8318d2188d2fc0c19ff72883a1f668745a79
Igor <Igor_infopol@ukr.net>
2015-04-20 18:39:41 +0300

    Mailing proj added

---
+++ SDesk/Mailing/IMailReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mailing
+{
+  public  interface IMailReader
+    {
+        void Connect(MapConfig config);
+        void Authentificate(MapConfig config);
+        void DeleteMessage(uint uid);
+        event NewMailDelagate NewMailArrived;
+    }
+}
