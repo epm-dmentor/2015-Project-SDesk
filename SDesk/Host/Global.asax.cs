67d6b24b422e307380f34962da3ba7259a18bbb4
bayramo <bayramchik@gmail.com>
2015-04-30 18:06:11 +0300

    Host Project draft was added for WebApi 2 and MVC Test controller

--- SDesk/Host/Global.asax.cs
+++ SDesk/Host/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Web.Http;
+using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace Epam.Sdesk.Host
@@ -8,6 +9,7 @@ namespace Epam.Sdesk.Host
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
